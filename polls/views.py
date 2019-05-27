fcce12ec6ce9bae07d51c3dbb7da820758e280aa
xiang <ixiangyantao@gmail.com>
2019-05-27 20:18:57 +0800

    stable

--- polls/views.py
+++ polls/views.py
@@ -5,26 +5,31 @@ from django.http import HttpResponse
 from .models import Question, Choice
 from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    print(request.path)
-    return HttpResponse(template.render(context))
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    def get_queryset(self):
+        """Return the last five published questions (not including those set to be
+        published in the future)."""
+        return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -34,7 +39,7 @@ def vote(request, question_id):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didn't select a choice.",
+            'error_message': "请至少选择一项！",
         })
     else:
         selected_choice.votes += 1
